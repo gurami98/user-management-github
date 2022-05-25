4c32376b48220add7f36c7c876d892b65b502781
gurami98 <chilindrishviligurami@gmail.com>
2022-05-26 03:34:45 +0400

    turn on strict mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext";
 
 createRoot(document.querySelector("#root")).render(
-  <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </BrowserRouter>
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </BrowserRouter>
+    </React.StrictMode>
 );
